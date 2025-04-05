d9d91d2690c02f13b66c8b6c23564838526aa02e
Mertozturkk <ozturkmmertt@gmail.com>
2025-04-05 13:17:10 +0300

    feat:header control

--- app/server.go
+++ app/server.go
@@ -47,27 +47,89 @@ func HandleFunction(conn net.Conn) {
 			return
 		}
 
-		RequestPathSplit(receiveMessage, conn)
+		req, err := NewRequest(buf[:n])
+		if err != nil {
+			fmt.Println("Error parsing request:", err)
+			conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+			return
+		}
+		fmt.Println("REEEQUEST:", req)
+
+		if req.Path == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		} else if strings.HasPrefix(req.Path, "/echo") {
+			message := strings.Split(req.Path, "/")[2]
+			fmt.Println("message", message)
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
+		} else if strings.HasPrefix(req.Path, "/user-agent") {
+			userAgent := req.Headers["User-Agent"]
+			fmt.Println("User-Agent>>>>>>>>>>>>>>>>:", userAgent)
+			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
+		} else {
+			fmt.Println("404 Not Found")
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
+
+		// RequestPathSplit(receiveMessage, conn)
 
 	}
 }
 
-func RequestPathSplit(receiveMessage string, conn net.Conn) {
-	splitedMessage := strings.Split(receiveMessage, "\r\n")[0]
-	fmt.Println("splitedMessage", splitedMessage)
-	targetPath := strings.Split(splitedMessage, " ")[1]
-	fmt.Println("targetPath", targetPath)
+// func RequestPathSplit(receiveMessage string, conn net.Conn) {
+// 	splitedMessage := strings.Split(receiveMessage, "\r\n")[0]
+// 	fmt.Println("splitedMessage", splitedMessage)
+// 	targetPath := strings.Split(splitedMessage, " ")[1]
+// 	fmt.Println("targetPath", targetPath)
 
-	if targetPath == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK" + "\r\n\r\n"))
-	} else if strings.HasPrefix(targetPath, "/echo") {
-		message := strings.Split(targetPath, "/")[2]
-		fmt.Println("message", message)
-		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
-	} else {
-		fmt.Println("404 Not Found")
-		conn.Write([]byte("HTTP/1.1 404 Not Found" + "\r\n\r\n"))
+// 	if targetPath == "/" {
+// 		conn.Write([]byte("HTTP/1.1 200 OK" + "\r\n\r\n"))
+// 	} else if strings.HasPrefix(targetPath, "/echo") {
+// 		message := strings.Split(targetPath, "/")[2]
+// 		fmt.Println("message", message)
+// 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
+// 	} else {
+// 		fmt.Println("404 Not Found")
+// 		conn.Write([]byte("HTTP/1.1 404 Not Found" + "\r\n\r\n"))
 
+// 	}
+
+// }
+
+type Request struct {
+	Method  string
+	Path    string
+	Headers map[string]string
+	Body    string
+}
+
+func NewRequest(b []byte) (*Request, error) {
+	request := &Request{
+		Headers: make(map[string]string),
 	}
 
+	lines := strings.Split(string(b), "\r\n")
+	if len(lines) < 1 {
+		return nil, errors.New("invalid request")
+	}
+
+	requestLine := strings.Split(lines[0], " ")
+	if len(requestLine) < 2 {
+		return nil, errors.New("invalid request line")
+	}
+
+	request.Method = requestLine[0]
+	request.Path = requestLine[1]
+
+	for _, line := range lines[1:] {
+		if line == "" {
+			break
+		}
+		header := strings.Split(line, ": ")
+		if len(header) != 2 {
+			return nil, errors.New("invalid header")
+		}
+		request.Headers[header[0]] = header[1]
+	}
+
+	return request, nil
 }
